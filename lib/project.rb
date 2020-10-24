dcb895c9cead88d22f69a4dc43a39e32788a9360
Michael Kim <m.kim4247@gmail.com>
2020-10-24 20:26:32 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,12 +8,6 @@ class Project
   
   def add_backer(backer)
     @backers << backer
-    if !backer.backed_projects.include?(backer)
-      backer.back_project(self)
-    end
-  end
-  
-  def backers
-    @backers
+    backer.
   end
 end
