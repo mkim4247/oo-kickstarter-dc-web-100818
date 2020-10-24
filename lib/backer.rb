13dec2d832b6829b110c03f476791ccac6db586e
Michael Kim <m.kim4247@gmail.com>
2020-10-24 20:20:01 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,14 @@
 class Backer
-  attr_accessor :backed_projects, :name 
+  attr_reader :backed_projects, :name
   
   def initialize(name)
-    @name = name 
-    @backed_projects =[]
-  end 
+    @name = name
+    @backed_projects = []
+  end
   
   def back_project(project)
-    @backed_projects << project 
-    project.backers << self 
-  end 
-  
-end 
+    @backed_projects << project
+    project.add_backer(self)
+  end
+
+end
--- lib/project.rb
+++ lib/project.rb
@@ -1,14 +1,14 @@
 class Project
-  attr_accessor :backers, :title 
-   
-   def initialize(title)
-     @title = title 
-     @backers = []
-   end 
-   
-   def add_backer(backer)
-     @backers << backer 
-      backer.backed_projects << self 
-   end 
-   
-end 
+  attr_reader :backers, :title
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def add_backer(backer)
+    @backers << backer
+    #backer.back_project(self)
+  end
+  
+end
